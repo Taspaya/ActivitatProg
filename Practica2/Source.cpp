d15ac3f0c66bdebb9d01d8cce26c35e12f9e1564
Gerard Ardit Castells <gerard.ardit@hotmail.com>
2017-10-19 11:07:08 +0200

    readyToCode

---
+++ Practica2/Source.cpp
@@ -0,0 +1,8 @@
+#include <SDL.h>		// Always needs to be included for an SDL app
+#include <SDL_image.h>  //Libreria que controla imagenes
+#include <SDL_ttf.h>	//libreria de texto
+#include <SDL_mixer.h>  //libreria de musica
+#include <time.h>
+#include <iostream>
+
+
